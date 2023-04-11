2eeadbf025643a5c039d4eac711e62b100d95ac7
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2023-04-11 21:13:23 +0200

    Create info.sh

---
+++ info.sh
@@ -0,0 +1,1 @@
+echo "alias lvs-cache='lvs -a -o +devices,cache_total_blocks,cache_used_blocks,cache_dirty_blocks,cache_read_hits,cache_read_misses,cache_write_hits,cache_write_misses,segtype'" >> ~/.bashrc
